db8a6cfd62c5f73d054838104013be02b68bb880
Alena <96778476+Alena307@users.noreply.github.com>
2021-12-29 12:28:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# Write your code below this line 
+bill = input("What are your parking costs?")
+bill1 = int(bill)
+people = input("How many people pay the bill?")
+people1 = int(people)
+#print(people)
+result = round(bill1 / people1,2)
+#print(result)
+print(f"Everyone should pay {result} Euros.\nLet's go to the parking slot mashine.")
